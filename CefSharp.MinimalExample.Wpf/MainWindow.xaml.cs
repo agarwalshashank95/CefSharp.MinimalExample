e689d2e03f0953bcb2bcf0b2e0b18d695751ef96
Shashank Agarwal <Shashank.Agarwal@microsoft.com>
2019-06-28 15:01:14 +0530

    Changed WPF to WinForm using WinFormHost

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,27 @@
 ﻿using System.Windows;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms.Integration;
 
 namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
+		WindowsFormsHost host = null;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
+
+			ChromiumWebBrowser chromeBr = new ChromiumWebBrowser();
+
+
+			host = new WindowsFormsHost();
+			host.Child = chromeBr;
+
+			chromeBr.Load("https://www.google.com");
+
+			BrowserContainer.Content = host;
+		}
     }
 }
